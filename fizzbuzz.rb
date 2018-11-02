cd84b1ec0b4e0b45420e5683d916280c2ce96de5
Ezenwa Onukwue <ezenwa.onukwue@salesloft.com>
2018-11-02 21:22:33 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
-def fizzbuzz
-if int == 3
+def fizzbuzz(int)
+if int % 3 == 0
   return "Fizz"
-elsif int == 5
+elsif int % 5 == 0
   return "Buzz"
-elsif int == 15
+elsif int % 3 && int % 5 == 0
   return "FizzBuzz"
 else
   return nil
